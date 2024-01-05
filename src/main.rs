bb1a8a273705fb904e36af981f7c05614b6a5787
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-01-06 02:39:14 +0530

    API call complete, response error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::string;
+use std::{string, collections::HashMap};
 
 slint::include_modules!();
+use std::collections::HashMap;
+
 
 //bussiness logic in this case compiling the code
 fn main() -> Result<(), slint::PlatformError> {
@@ -10,8 +12,20 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.compile_code_rs(move |string,string,string| {
         let ui = ui_handle.unwrap();
 
+        let URL = "https://play.rust-lang.org/execute";
+        let mut data : HashMap<&str,&str> = HashMap::new();
+        data.insert("channel", "stable");
+        data.insert("mode", "debug");
+        data.insert("edition", "2024");
+        data.insert("crateType", "bin");
+        data.insert("tests", false);
+        data.insert("code", ui.get_code());
+        data.insert("backtrace", false);
+
         //compile code here and change the property likewise
-        ui.set_counter(ui.get_counter() + 1);
+        ui.set_stderr();
+        ui.set_stdout();
+
     });
 
     ui.run()
