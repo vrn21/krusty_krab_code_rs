cb16ea5b7f80416adb6b5347fa2bbc46e74abc83
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-01-07 16:05:27 +0530

    Frontend blank screen fixed

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,19 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.compile_code_rs(move |string,string,string| {
         let ui = ui_handle.unwrap();
 
-        let URL = "https://play.rust-lang.org/execute";
-        let mut data : HashMap<&str,&str> = HashMap::new();
-        data.insert("channel", "stable");
-        data.insert("mode", "debug");
-        data.insert("edition", "2024");
-        data.insert("crateType", "bin");
-        data.insert("tests", false);
-        data.insert("code", ui.get_code());
-        data.insert("backtrace", false);
+        // let URL = "https://play.rust-lang.org/execute";
+        // let mut data : HashMap<&str,&str> = HashMap::new();
+        // data.insert("channel", "stable");
+        // data.insert("mode", "debug");
+        // data.insert("edition", "2024");
+        // data.insert("crateType", "bin");
+        // data.insert("tests", false);
+        // data.insert("code", ui.get_code());
+        // data.insert("backtrace", false);
 
         //compile code here and change the property likewise
-        ui.set_stderr();
-        ui.set_stdout();
+        // ui.set_stderr();
+        // ui.set_stdout();
 
     });
 
