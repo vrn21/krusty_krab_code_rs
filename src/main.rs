d86c76b8404e7ffbce7a5c9e69c52126aa5b48a2
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-01-07 16:32:57 +0530

    Made changes to main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::{string, collections::HashMap};
+use piston_rs;
+use reqwest::Response;
 
 slint::include_modules!();
 use std::collections::HashMap;
@@ -7,10 +9,32 @@ use std::collections::HashMap;
 //bussiness logic in this case compiling the code
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-
     let ui_handle = ui.as_weak();
     ui.compile_code_rs(move |string,string,string| {
         let ui = ui_handle.unwrap();
+        let client = piston_rs::Client::new();
+        let executor = piston_rs::Executor::new()
+            .set_language("rust")
+            .set_version("*")
+            .add_file(piston_rs::File::default()
+                .set_name("main.rs")
+                .set_content(ui.get_code)
+            );
+
+        match client.execute(&executor)    {
+            Ok(response) => {
+                print!("Language : Rust {}",response.version);
+                if let Some(c) = &response.compile{
+                    ui.set_stderr(response.compile)
+                }
+                ui.set_stdout(response.run.output);
+            }
+            Err(err) => {
+                ui.set_stderr("Something bad happend, its from our side, sorry, {:?}",err);
+            }
+        }
+
+        
 
         // let URL = "https://play.rust-lang.org/execute";
         // let mut data : HashMap<&str,&str> = HashMap::new();
